12ca938c727c5be7a5fd5064db50e955a6183786
SantiagoLpzz <22014@portalcedi.edu.mx>
2025-01-23 08:06:43 -0600

    cambios defauly

---
+++ QuizzIonic/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'QuizzIonic',
+  webDir: 'www'
+};
+
+export default config;
